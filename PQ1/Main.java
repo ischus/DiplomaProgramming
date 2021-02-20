a2f205a366be892407b43c79dcf6529ca5b2ffb9
ischus <Atuitupou2@gmail.com>
2021-02-20 12:46:17 +0800

    Update Main.java

--- PQ1/Main.java
+++ PQ1/Main.java
@@ -1,28 +1,35 @@
 package com.company;
 import org.w3c.dom.Node;
 import java.util.Scanner;
+//Imports country class with city
 import com.company.Country;
 
 public class Main
 {
-
+    //Creates the doubly link list structure
     public static class DoublyLinkedList
     {
         public class Node
         {
+            //Once a node is created the information is entered into the node
             Country country = new Country();
             Country.City city = country.new City();
-
+            
             Node next;
             Node prev;
 
         }
+        
+        //Sets the node head and tail to null to prepare the head and tail to connect
+        //to the new head and tail
         Node head = null;
         Node tail = null;
 
         public void addNode()
         {
             Node newNode = new Node();
+            //If the head after the new node has been created or the first node than
+            //resign the head and tail
             if(head == null)
             {
                 head = tail = newNode;
@@ -37,15 +44,17 @@ public class Main
                 tail.next = null;
             }
         }
-
+        //Prints information of country and city
         public void printNodes()
         {
+            //Checks if the list is empty and prevents printing null values
             Node current = head;
             if(head == null)
             {
                 System.out.println("Doubly linked list is empty");
                 return;
             }
+            //Otherwise print the linked list
             System.out.println("Linked list is as follows: ");
             while(current != null)
             {
