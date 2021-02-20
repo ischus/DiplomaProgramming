bea3f67bbe8b6742f642ffd3cdc7cd1d7e00df82
ischus <Atuitupou2@gmail.com>
2021-02-20 12:48:22 +0800

    Update Country.java

--- PQ1/Country.java
+++ PQ1/Country.java
@@ -3,20 +3,27 @@ import java.util.Scanner;
 
 public class Country
 {
+    //The string value takes the name of the country
     String name;
+    //Once a new node is created ask the user for input for that node
     public Country()
     {
+        //Scan user input and place into node
         Scanner sc1 = new Scanner(System.in);
         System.out.println("Please enter country name");
         name = sc1.next();
 
     }
-
+    
+    
     public class City
     {
+        //The string value takes the name of the city
         String name;
+        //Once a new node is created ask the user for input for that node
         public City()
         {
+            //Scan user input and place into node
             Scanner sc1 = new Scanner(System.in);
             System.out.println("Please enter city name");
             name = sc1.next();
